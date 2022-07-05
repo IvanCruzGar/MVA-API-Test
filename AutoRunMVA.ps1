46bd76f85660fdfbea14bab1554b04864c548156
IvanC <ivan.cruz@aspentech.com>
2022-07-05 03:29:23 +0000

    Autorun edited

--- AutoRunMVA.ps1
+++ AutoRunMVA.ps1
@@ -4,14 +4,12 @@ $pytest_A = "pytest -m mva_ProcessPulseV14 --csv Report1.csv --csv-columns marke
 
 
 
-#Executing the Python 3.7.8 version into the VM
-$ipv4=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "Ethernet 3").IPAddress
-(Get-Content .\hosts_config.py).Replace('REEMPLAZAR',$ipv4) | Out-File .\hosts_config.py
+
 
 Start-Process "cmd.exe"  "/k $pytest_A"
-while (!(Test-Path "C:\p4\MVA-API-Test\Report1.csv")){ Start-Sleep 20}
+while (!(Test-Path ".\Report1.csv")){ Start-Sleep 20}
 
-Import-Csv -Path 'C:\p4\MVA-API-Test\Report1.csv' | ForEach-Object {
+Import-Csv -Path '.\Report1.csv' | ForEach-Object {
     If ($_.status -eq 'failed') {
         $_.status = 'Fail'
     }
@@ -27,19 +25,11 @@ Import-Csv -Path 'C:\p4\MVA-API-Test\Report1.csv' | ForEach-Object {
         $_.markers = $_.markers.trim('mva_ProcessPulseV14,vsts')
     }
     $_
-}  | ForEach-Object {
-    
-    Write-Debug $_.duration
-    $_
-} | Export-Csv -Path 'C:\p4\MVA-API-Test\Report2.csv' -NoTypeInformation 
+}  | Export-Csv -Path '.\Report2.csv' -NoTypeInformation 
 
 
-$csv = 'C:\p4\MVA-API-Test\Report2.csv'
+$csv = '.\Report2.csv'
 (Get-Content $csv) -replace '"', '' |
-    Set-Content $csv
-
-$csv = 'C:\p4\MVA-API-Test\Report2.csv'
-(Get-Content $csv) -replace '`r', '' |
     Set-Content $csv
 
 #quedo el push batoo
